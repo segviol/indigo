c694ac01d94070423c768c442e6bdc9735828cbd
Rynco Maekawa <lynzrand@outlook.com>
2020-06-17 16:13:32 +0800

    Basic instruction declaration

--- backend/mir/mir.hpp
+++ backend/mir/mir.hpp
@@ -1,5 +1,81 @@
 #pragma once
+#include <vector>
+using namespace std;
 
-namespace mir {
-class Mir {};
-}  // namespace mir
+namespace mir::inst {
+
+enum class InstKind { Assign, Op, Ref, Deref, Call, Phi };
+
+enum class Op { Add, Sub, Mul, Div, Gt, Lt, Gte, Lte, Eq, Neq };
+
+class Variable {};
+
+class Value {};
+
+/// Base class for instruction
+class Inst {
+ public:
+  /// Instruction destination variable
+  Variable dest;
+
+  virtual InstKind inst_kind();
+};
+
+/// Assign instruction. `$dest = $src`
+class AssignInst : Inst {
+ public:
+  Value src;
+
+  virtual InstKind inst_kind() { return InstKind::Assign; }
+};
+
+/// Operator instruction. `$dest = $lhs op $rhs`
+class OpInst : Inst {
+ public:
+  Value lhs;
+  Value rhs;
+  Op op;
+
+  virtual InstKind inst_kind() { return InstKind::Op; }
+};
+
+/// Call instruction. `$dest = call $func(...$params)`
+class CallInst : Inst {
+ public:
+  Variable func;
+  vector<Value> params;
+
+  virtual InstKind inst_kind() { return InstKind::Call; }
+};
+
+/// Reference instruction. `$dest = &$val`
+class RefInst : Inst {
+ public:
+  Variable val;
+
+  virtual InstKind inst_kind() { return InstKind::Ref; }
+};
+
+/// Dereference instruction. `$dest = *$val`
+class DerefInst : Inst {
+ public:
+  Value val;
+
+  virtual InstKind inst_kind() { return InstKind::Deref; }
+};
+
+/// Phi instruction. `$dest = phi(...$vars)`
+class PhiInst : Inst {
+ public:
+  vector<Variable> vars;
+
+  virtual InstKind inst_kind() { return InstKind::Phi; }
+};
+
+}  // namespace mir::inst
+
+namespace mir::codegen {
+
+class CodeGen {};
+
+}  // namespace mir::codegen
