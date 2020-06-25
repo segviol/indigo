066982f344e01f3775ed1377753d91bc4a620e7c
root <@root>
2020-06-25 15:51:38 +0800

    finish word anlyze
    词法分析的相关代码全在front:::word命名空间下，实现词法分析的方式为函数。
    创建新的词法类，可能需要根据情况调整词法类。

--- frontend/README.md
+++ frontend/README.md
@@ -1,2 +1,43 @@
 <h1 align="center">编译器前端</h1>
 
+<h2>一、词法分析</h2>
+
+<h3>1. 终结符Token名对应表</h3>
+
+| 序号 | 名称       | 内容       |
+| ---- | ---------- | ---------- |
+| 1    | IDENFR     | 标识符     |
+| 2    | INTCON     | 数值常量   |
+| 3    | STRCON     | 字符串常量 |
+| 4    | CONSTTK    | const      |
+| 5    | INTTK      | int        |
+| 6    | VOIDTK     | void       |
+| 7    | IFTK       | if         |
+| 8    | ELSETK     | else       |
+| 9    | WHILETK    | while      |
+| 10   | BREAKTK    | break      |
+| 11   | CONTINUETK | continue   |
+| 12   | RETURNTK   | return     |
+| 13   | PLUS       | +          |
+| 14   | MINU       | -          |
+| 15   | MULT       | *          |
+| 16   | DIV        | /          |
+| 17   | MOD        | %          |
+| 18   | LSS        | <          |
+| 19   | LEQ        | <=         |
+| 20   | GRE        | >          |
+| 21   | GEQ        | >=         |
+| 22   | EQL        | ==         |
+| 23   | NEQ        | !=         |
+| 24   | AND        | &&         |
+| 25   | OR         | \|\|       |
+| 26   | NOT        | !          |
+| 27   | ASSIGN     | =          |
+| 28   | SEMICN     | ;          |
+| 29   | COMMA      | ,          |
+| 30   | LPARENT    | (          |
+| 31   | RPARENT    | )          |
+| 32   | LBRACK     | [          |
+| 33   | RBRACK     | ]          |
+| 34   | LBRACE     | {          |
+| 35   | RBRACE     | }          |
---
+++ frontend/word_analyze.hpp
@@ -0,0 +1,92 @@
+#pragma once
+#include <string>
+#include <ctype.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+#ifndef COMPILER_FRONT_WORD_H_
+#define COMPILER_FRONT_WORD_H_
+
+namespace front::word {
+    using std::string;
+    using std::vector;
+    using std::hash;
+    using std::isalpha;
+    using std::isdigit;
+
+    enum class Token
+    {
+        IDENFR, INTCON, STRCON,
+        CONSTTK, INTTK, VOIDTK,
+        IFTK, ELSETK, WHILETK,
+        BREAK, CONTINUETK, RETURNTK,
+        PLUS, MINU, MULT,
+        DIV, MOD, LSS,
+        LEQ, GRE, GEQ,
+        EQL, NEQ, AND,
+        OR, NOT, ASSIGN,
+        SEMICN, COMMA, LPARENT,
+        RPARENT, LBRACK, RBRACK,
+        LBRACE, RBRACE
+    };
+
+    const string Token_strs[] = {
+        "IDENFR", "INTCON", "STRCON",
+        "CONSTTK","INTTK", "VOIDTK",
+        "IFTK", "ELSETK", "WHILETK",
+        "BREAK", "CONTINUETK", "RETURNTK",
+        "PLUS", "MINU", "MULT",
+        "DIV", "MOD", "LSS",
+        "LEQ", "GRE", "GEQ",
+        "EQL", "NEQ", "AND",
+        "OR", "NOT", "ASSIGN",
+        "SEMICN", "COMMA", "LPARENT",
+        "RPARENT", "LBRACK", "RBRACK",
+        "LBRACE", "RBRACE"
+    };
+
+    inline bool octal_digit(char ch);
+    inline bool hex_digit(char ch);
+    inline bool is_letter(char c);
+    inline bool have_body_char(char c);
+    inline void get_char(char& ch, int& index, const string& str);
+    inline void retract(char& ch, int& index, const string& str);
+    inline bool is_illegel_char(char ch);
+    Token get_token(const string& buf);
+
+    #define VECTOR_SIZE 1024
+
+    class Word {
+    public:
+        Word();
+        Word(Token in_token_value, string in_self, unsigned int in_line_num);
+        Token get_token_value();
+        string get_token_string();
+        string get_self() const;
+        bool match_token(Token tk) const;
+        unsigned int get_line_num() const;
+    private:
+        Token token_value;
+        string self;
+        unsigned int line_num;
+    };
+
+    struct WordHash
+    {
+        size_t operator()(const Word& word) const {
+            return hash<string>()(word.get_self());
+        }
+    };
+
+    struct WordCmp
+    {
+        bool operator()(const Word& a, const Word& b) const {
+            return a.get_self() == b.get_self();
+        }
+    };
+
+    void word_analyse(string& input_str, vector<Word>& word_arr);
+}
+
+#endif // !COMPILER_FRONT_WORD_H_
