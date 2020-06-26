9d27d46bbf0437312cec64f986ed64c2d323a51e
Rynco Maekawa <lynzrand@outlook.com>
2020-06-26 16:38:11 +0800

    Initial definition of arm assembly

--- prelude/prelude.hpp
+++ prelude/prelude.hpp
@@ -1,7 +1,26 @@
 #pragma once
+#include <climits>
 #include <iostream>
 
 namespace prelude {
+
+static inline uint32_t rotl32(uint32_t n, unsigned int c) {
+  const unsigned int mask =
+      (CHAR_BIT * sizeof(n) - 1);  // assumes width is a power of 2.
+
+  // assert ( (c<=mask) &&"rotate by type width or more");
+  c &= mask;
+  return (n << c) | (n >> ((-c) & mask));
+}
+
+static inline uint32_t rotr32(uint32_t n, unsigned int c) {
+  const unsigned int mask = (CHAR_BIT * sizeof(n) - 1);
+
+  // assert ( (c<=mask) &&"rotate by type width or more");
+  c &= mask;
+  return (n >> c) | (n << ((-c) & mask));
+}
+
 class Displayable {
  public:
   virtual void display(std::ostream& o) const = 0;
