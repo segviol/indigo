deed8fc1c6473e8cae76a62fd3b44802ee8103ef
Z <tqnwhz@qq.com>
2020-06-28 17:21:02 +0800

    modify gitignore

---
+++ CMakeFiles/compiler.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tqnwhz/compiler/main.cpp" "/home/tqnwhz/compiler/CMakeFiles/compiler.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/tqnwhz/compiler/CMakeFiles/backend.dir/DependInfo.cmake"
+  "/home/tqnwhz/compiler/CMakeFiles/mir.dir/DependInfo.cmake"
+  "/home/tqnwhz/compiler/CMakeFiles/arm.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/mir.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mir.
+# This may be replaced when dependencies are built.
