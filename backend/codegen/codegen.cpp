8c35c07e25c0e865027396c82fa5b46e5fdf98e2
Rynco Maekawa <lynzrand@outlook.com>
2020-06-28 17:40:10 +0800

    Infrastructure on mir and arm code

--- backend/codegen/codegen.cpp
+++ backend/codegen/codegen.cpp
@@ -1,29 +1,122 @@
 #include "codegen.hpp"
 
+#include <cassert>
 #include <exception>
 #include <memory>
+#include <typeinfo>
 
 namespace backend::codegen {
 
-arm::ArmCode Codegen::translate() { throw new std::exception(); }
+using namespace arm;
 
-arm::Function Codegen::translate_function(mir::inst::MirFunction& f) {
-  return arm::Function();
-}
+arm::Function Codegen::translate_function() { return arm::Function(); }
 
 void Codegen::translate_basic_block(mir::inst::BasicBlk& blk) {
   for (auto& inst : blk.inst) {
     auto& i = *inst;
     if (auto x = dynamic_cast<mir::inst::OpInst*>(&i)) {
+      translate_inst(*x);
     } else if (auto x = dynamic_cast<mir::inst::CallInst*>(&i)) {
+      translate_inst(*x);
     } else if (auto x = dynamic_cast<mir::inst::AssignInst*>(&i)) {
+      translate_inst(*x);
     } else if (auto x = dynamic_cast<mir::inst::LoadInst*>(&i)) {
+      translate_inst(*x);
     } else if (auto x = dynamic_cast<mir::inst::StoreInst*>(&i)) {
+      translate_inst(*x);
     } else if (auto x = dynamic_cast<mir::inst::RefInst*>(&i)) {
+      translate_inst(*x);
     } else if (auto x = dynamic_cast<mir::inst::PhiInst*>(&i)) {
+      translate_inst(*x);
     } else if (auto x = dynamic_cast<mir::inst::PtrOffsetInst*>(&i)) {
+      translate_inst(*x);
+    } else {
+      throw new std::bad_cast();
     }
   }
 }
+
+arm::Reg Codegen::get_or_alloc_vgp(mir::inst::VarId v) {
+  auto found = reg_map.find(v);
+  if (found != reg_map.end()) {
+    assert(arm::register_type(found->second) ==
+           arm::RegisterKind::VirtualGeneralPurpose);
+    return found->second;
+  } else {
+    return alloc_vgp();
+  }
+}
+
+arm::Reg Codegen::get_or_alloc_vd(mir::inst::VarId v) {
+  auto found = reg_map.find(v);
+  if (found != reg_map.end()) {
+    assert(arm::register_type(found->second) ==
+           arm::RegisterKind::VirtualDoubleVector);
+    return found->second;
+  } else {
+    return alloc_vd();
+  }
+}
+
+arm::Reg Codegen::get_or_alloc_vq(mir::inst::VarId v) {
+  auto found = reg_map.find(v);
+  if (found != reg_map.end()) {
+    assert(arm::register_type(found->second) ==
+           arm::RegisterKind::VirtualQuadVector);
+    return found->second;
+  } else {
+    return alloc_vq();
+  }
+}
+
+arm::Reg Codegen::alloc_vgp() {
+  return arm::make_register(arm::RegisterKind::VirtualGeneralPurpose,
+                            vreg_gp_counter++);
+}
+
+arm::Reg Codegen::alloc_vd() {
+  return arm::make_register(arm::RegisterKind::VirtualDoubleVector,
+                            vreg_gp_counter++);
+}
+
+arm::Reg Codegen::alloc_vq() {
+  return arm::make_register(arm::RegisterKind::VirtualQuadVector,
+                            vreg_gp_counter++);
+}
+
+arm::Operand2 Codegen::translate_operand2(mir::inst::Value& v) {
+  // TODO
+  throw new std::exception();
+}
+
+arm::Reg Codegen::translate_operand2_alloc_reg(mir::inst::Value& v) {
+  // TODO
+  throw new std::exception();
+}
+
+arm::Reg Codegen::translate_var_reg(mir::inst::VarId v) {
+  // TODO
+  throw new std::exception();
+}
+
+void Codegen::translate_inst(mir::inst::AssignInst& i) {
+  inst.push_back(
+      std::make_unique<Arith2Inst>(arm::OpCode::Mov, translate_var_reg(0),
+                                   translate_operand2_alloc_reg(i.src)));
+}
+
+void Codegen::translate_inst(mir::inst::PhiInst& i) {}
+
+void Codegen::translate_inst(mir::inst::CallInst& i) {}
+
+void Codegen::translate_inst(mir::inst::StoreInst& i) {}
+
+void Codegen::translate_inst(mir::inst::LoadInst& i) {}
+
+void Codegen::translate_inst(mir::inst::RefInst& i) {}
+
+void Codegen::translate_inst(mir::inst::PtrOffsetInst& i) {}
+
+void Codegen::translate_inst(mir::inst::OpInst& i) {}
 
 }  // namespace backend::codegen
--- backend/codegen/codegen.hpp
+++ backend/codegen/codegen.hpp
@@ -7,16 +7,36 @@ namespace backend::codegen {
 
 class Codegen final {
  public:
-  Codegen(mir::inst::MirPackage& pkg) : package(pkg) {}
+  Codegen(mir::inst::MirFunction& func,
+          std::map<std::string, std::any>& extra_data)
+      : func(func), extra_data(extra_data), inst() {}
 
   /// Translate MirPackage into ArmCode
-  arm::ArmCode translate();
+  arm::Function translate_function();
 
  private:
-  mir::inst::MirPackage& package;
-  std::vector<std::unique_ptr<arm::Inst>> inst;
+  mir::inst::MirFunction& func;
+  std::map<std::string, std::any>& extra_data;
 
-  arm::Function translate_function(mir::inst::MirFunction& f);
+  std::vector<std::unique_ptr<arm::Inst>> inst;
+  std::map<mir::inst::VarId, arm::Reg> reg_map;
+
+  uint32_t vreg_gp_counter = 0;
+  uint32_t vreg_vd_counter = 0;
+  uint32_t vreg_vq_counter = 0;
+
+  arm::Reg get_or_alloc_vgp(mir::inst::VarId v);
+  arm::Reg get_or_alloc_vd(mir::inst::VarId v);
+  arm::Reg get_or_alloc_vq(mir::inst::VarId v);
+  arm::Reg alloc_vgp();
+  arm::Reg alloc_vd();
+  arm::Reg alloc_vq();
+
+  arm::Operand2 translate_operand2(mir::inst::Value& v);
+  arm::Reg translate_operand2_alloc_reg(mir::inst::Value& v);
+  arm::Reg translate_var_reg(mir::inst::VarId v);
+
+  // arm::Function translate_function(mir::inst::MirFunction& f);
   void translate_basic_block(mir::inst::BasicBlk& blk);
   void translate_inst(mir::inst::AssignInst& i);
   void translate_inst(mir::inst::PhiInst& i);
