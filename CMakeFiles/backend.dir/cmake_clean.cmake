26b93f6622c6551a714a9854c863b2423c360250
Z <tqnwhz@qq.com>
2020-06-28 17:21:02 +0800

    modify gitignore

---
+++ CMakeFiles/backend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backend.dir/backend/codegen/codegen.cpp.o"
+  "libbackend.pdb"
+  "libbackend.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/mir.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mir.dir/mir/mir.cpp.o"
+  "libmir.pdb"
+  "libmir.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mir.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
