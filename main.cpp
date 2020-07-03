c7a935fe201f62a7c310642c5ee5d1c5c6230778
Rynco Maekawa <lynzrand@outlook.com>
2020-07-03 22:12:39 +0800

    change func and const id to string & fuck c++ compiler

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+#include "backend/backend.hpp"
+#include "backend/codegen/codegen.hpp"
+#include "backend/optimization/graph_color.hpp"
 #include "mir/mir.hpp"
+// #include "prelude/fake_mir_generate.hpp"
 
 int main(int argc, char const *argv[]) {
   // TODO: code
+  // front::fake::FakeGenerator x;
+  // x.fakeMirGenerator1();
+  // auto mir = x._package;
+
+  // backend::Backend backend(std::move(*mir));
+  // auto code = backend.generate_code();
 
   return 0;
 }
