3238923c1425693bff38987093d2cb04cf42f7c4
hyc <hyc2026@yeah.net>
2020-07-06 01:37:52 +0800

    pass function_test 1-9

--- frontend/optim_mir.hpp
+++ frontend/optim_mir.hpp
@@ -7,8 +7,10 @@
 #include <memory>
 #include <algorithm>
 #include <queue>
+#include <iostream>
 
 #include "../mir/mir.hpp"
+#include "ir_generator.hpp"
 
 #ifndef COMPILER_FRONT_OPTIM_MIR_H_
 #define COMPILER_FRONT_OPTIM_MIR_H_
@@ -21,40 +23,15 @@ namespace front::optim_mir {
         JumpLableId(int jumpLabelId);
     };
 
-    /*class Inst {
-    public:
-        int s;
-        Inst(int _s) :s(_s) {};
-    };
-
-    class JumpInstruction {
-    public:
-        int kind;
-        int bb_true;
-        int bb_false;
-        JumpInstruction(int _kind, int _bb_true, int _bb_false)
-            :kind(_kind), bb_true(_bb_true), bb_false(_bb_false) {};
-    };*/
-
-    typedef std::variant<std::shared_ptr<mir::inst::Inst>,
-        std::shared_ptr<mir::inst::JumpInstruction>,
-        std::shared_ptr<JumpLableId>>
-        Instruction;
-
     class BasicBlock {
     public:
         int id;
         int pre_id;
-        std::vector<Instruction> inst;
+        std::vector<front::irGenerator::Instruction> inst;
         std::vector<BasicBlock*> nextBlock;
         std::vector<BasicBlock*> preBlock;
         BasicBlock(int _id, int _pre_id);
     };
-
-    //Results of intermediate code generation
-    //std::map<int, std::vector<Instruction>> _funcIdToInstructions;
-
-    //map<int, Node> generate_CFG(vector<Instruction> instructions);
 
     class phi_index {
     public:
@@ -77,32 +54,13 @@ namespace front::optim_mir {
         int num;
     }phi_info;
 
-    //std::map<int, BasicBlock> front::optim_mir::generate_CFG(std::vector<Instruction> instructions);
 }
 
-extern std::vector<mir::inst::VarId> V;
-extern int num;
-extern std::vector<int> order;
+//extern std::vector<mir::inst::VarId> V;
+//extern int num;
+//extern std::vector<int> order;
+//extern int global_id;
 
-std::map<int, front::optim_mir::BasicBlock*> generate_CFG(std::vector<front::optim_mir::Instruction> instructions);
-front::optim_mir::BasicBlock* find_entry(std::map<int, front::optim_mir::BasicBlock*> nodes);
-front::optim_mir::BasicBlock* find_exit(std::map<int, front::optim_mir::BasicBlock*> nodes);
-std::vector<int> vectors_intersection(std::vector<int> v1, std::vector<int> v2);
-std::vector<int> vectors_set_union(std::vector<int> v1, std::vector<int> v2);
-std::vector<int> vectors_difference(std::vector<int> v1, std::vector<int> v2);
-bool has_this(std::vector<mir::inst::VarId> v, mir::inst::VarId s);
-std::vector<std::string> vectors_intersection(std::vector<std::string> v1, std::vector<std::string> v2);
-std::vector<std::string> vectors_set_union(std::vector<std::string> v1, std::vector<std::string> v2);
-std::vector<std::string> vectors_difference(std::vector<std::string> v1, std::vector<std::string> v2);
-std::vector<std::string> calcu_out(front::optim_mir::BasicBlock* b, std::map<int, std::vector<std::string>> in);
-std::vector<std::string> calcu_in(int n, std::map<int, std::vector<std::string>> use,
-    std::map<int, std::vector<std::string>> out, std::map<int, std::vector<std::string>> def);
-std::map<int, std::vector<mir::inst::VarId>> active_var(std::map<int, front::optim_mir::BasicBlock*> nodes);
-std::vector<int> pred_intersection(std::map<int, front::optim_mir::BasicBlock*> nodes, int n, std::map<int, std::vector<int>> dom);
-std::map<int, int> find_idom(std::map<int, front::optim_mir::BasicBlock*> nodes);
-std::map<int, std::vector<int>> dominac_frontier(std::map<int, int> dom, std::map<int, front::optim_mir::BasicBlock*> nodes);
-void generate_SSA(std::map<int, front::optim_mir::BasicBlock*> nodes, std::map<int,
-    std::vector<std::string>> global, std::map<int, std::vector<int>> dom_f, std::vector<front::optim_mir::Instruction> instructions);
-std::map<std::string, std::vector<front::optim_mir::Instruction>> gen_ssa(std::map<std::string, std::vector<front::optim_mir::Instruction>> f);
+std::map<std::string, std::vector<front::irGenerator::Instruction>> gen_ssa(std::map<std::string, std::vector<front::irGenerator::Instruction>> f);
 
 #endif
