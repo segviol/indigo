9d24e33e105c1f1c8f3c1bfc8f68153056679758
Rynco Maekawa <lynzrand@outlook.com>
2020-07-08 16:54:19 +0800

    Fix various issue in codegen

--- main.cpp
+++ main.cpp
@@ -64,10 +64,10 @@ int main(int argc, const char** argv) {
   backend::Backend backend(package, options);
 
   // backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>());
-  // backend.add_pass(
-  //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  // backend.add_pass(
-  //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
+  backend.add_pass(
+      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
+  backend.add_pass(
+      std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
   backend.add_pass(std::make_unique<backend::codegen::MathOptimization>());
 
   auto code = backend.generate_code();
