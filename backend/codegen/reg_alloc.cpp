c060523d29121651f854edd422ca772f927c7e72
Rynco Maekawa <lynzrand@outlook.com>
2020-07-09 14:15:32 +0800

    Fix compile error

--- backend/codegen/reg_alloc.cpp
+++ backend/codegen/reg_alloc.cpp
@@ -162,11 +162,11 @@ void RegAllocator::alloc_regs() {
 
   {
     // Add used reg stuff
-    auto first = f.inst.begin()->get();
-    auto first_ = static_cast<PushPopInst *>(first);
+    auto &first = f.inst.front();
+    auto first_ = static_cast<PushPopInst *>(&*first);
     for (auto r : used_regs) first_->regs.push_back(r);
-    auto last = f.inst.back()->get();
-    auto last_ = static_cast<PushPopInst *>(last);
+    auto &last = f.inst.back();
+    auto last_ = static_cast<PushPopInst *>(&*last);
     for (auto r : used_regs) last_->regs.push_back(r);
     f.inst.insert(f.inst.begin() + 2,
                   std::make_unique<Arith3Inst>(OpCode::Add, REG_SP, REG_SP,
