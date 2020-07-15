cfb89155866dd587b42d1cf97de8fd58bc6ef2d4
Z <tqnwhz@qq.com>
2020-07-16 07:37:30 +0800

    fix main

--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ int main(int argc, const char** argv) {
   //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   // backend.add_pass(
   //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
-  backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
+  // backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
   // backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>(5));
   backend.add_pass(std::make_unique<backend::codegen::MathOptimization>());
