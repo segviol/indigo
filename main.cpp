e74668b1c3f70ef734b6c10a629fbadfa16a9132
Rynco Maekawa <lynzrand@outlook.com>
2020-07-15 19:32:45 +0800

    Enable all passes

--- main.cpp
+++ main.cpp
@@ -68,12 +68,12 @@ int main(int argc, const char** argv) {
   backend::Backend backend(package, options);
 
   // backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>());
-  // backend.add_pass(
-  //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  // backend.add_pass(
-  //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
+  backend.add_pass(
+      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
+  backend.add_pass(
+      std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
-  // backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>(5));
+  backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>(5));
   backend.add_pass(std::make_unique<backend::codegen::MathOptimization>());
   backend.add_pass(std::make_unique<backend::codegen::RegAllocatePass>());
 
