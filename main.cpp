92fe019906d9df04cc595cd9780556d4667f5b4b
Rynco Maekawa <lynzrand@outlook.com>
2020-07-20 20:50:26 +0800

    Disable all optimizations

--- main.cpp
+++ main.cpp
@@ -67,10 +67,10 @@ int main(int argc, const char** argv) {
   LOG(INFO) << ("generating ARM code");
 
   backend::Backend backend(package, options);
-  backend.add_pass(
-      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
+  // backend.add_pass(
+  //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   // backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
-  backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
+  // backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
   // backend.add_pass(
   //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
