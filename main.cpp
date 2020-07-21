eed6e6b2d14af5565d018d1a159c680dede24382
Z <tqnwhz@qq.com>
2020-07-21 10:58:47 +0800

    open all passes

--- main.cpp
+++ main.cpp
@@ -67,16 +67,16 @@ int main(int argc, const char** argv) {
   LOG(INFO) << ("generating ARM code");
 
   backend::Backend backend(package, options);
-  // backend.add_pass(
-  //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  // backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
-  // backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
-  // backend.add_pass(
-  //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
-  // backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
-  // backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>(5));
-  // backend.add_pass(std::make_unique<backend::codegen::MathOptimization>());
-  // backend.add_pass(std::make_unique<backend::codegen::RegAllocatePass>());
+  backend.add_pass(
+      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
+  backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
+  backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
+  backend.add_pass(
+      std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
+  backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
+  backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>(5));
+  backend.add_pass(std::make_unique<backend::codegen::MathOptimization>());
+  backend.add_pass(std::make_unique<backend::codegen::RegAllocatePass>());
 
   auto code = backend.generate_code();
   if (options.verbose) {
