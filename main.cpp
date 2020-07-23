1d38ffbff8be480a8c45350edd04ce882c074b10
hyc <hyc2026@yeah.net>
2020-07-23 21:46:47 +0800

    Comment out inline

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main(int argc, const char** argv) {
   LOG(INFO) << "generating SSA" << std::endl;
 
   gen_ssa(inst, package, irgenerator);
-  //const_propagation(package);
+  const_propagation(package);
   
   // LOG(TRACE) << "Mir" << std::endl << package << std::endl;
   LOG(INFO) << ("Mir_Before") << std::endl;
@@ -72,7 +72,7 @@ int main(int argc, const char** argv) {
   backend::Backend backend(package, options);
   backend.add_pass(
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
+  //backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
   backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
   // backend.add_pass(
   //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
