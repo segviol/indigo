53f66a51f131e8a5e9c6ede5bd765ef6efa4d031
Z <tqnwhz@qq.com>
2020-07-23 20:39:32 +0800

    enable inline

--- main.cpp
+++ main.cpp
@@ -70,7 +70,7 @@ int main(int argc, const char** argv) {
   backend::Backend backend(package, options);
   backend.add_pass(
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  // backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
+  backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
   backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
   // backend.add_pass(
   //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
