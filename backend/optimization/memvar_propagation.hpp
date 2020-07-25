f93964b1201561b42b80dc4804fad63a3559d82e
hyc <hyc2026@yeah.net>
2020-07-25 19:40:12 +0800

    add const

--- backend/optimization/memvar_propagation.hpp
+++ backend/optimization/memvar_propagation.hpp
@@ -34,35 +34,44 @@ class Memory_Var_Propagation : public backend::MirOptimizePass {
       for (int j = 0; j < call_index.size(); j++) {
         int upper_bound =
             (j == (call_index.size() - 1)) ? bb.inst.size() : call_index[j + 1];
-        std::map<mir::inst::VarId, mir::inst::VarId> store;
-        std::map<mir::inst::VarId, mir::inst::VarId> load;
-        std::map<mir::inst::VarId, mir::inst::VarId>::iterator it;
+        std::map<mir::inst::VarId, std::variant<int32_t, mir::inst::VarId>>
+            store;
+        std::map<mir::inst::VarId, std::variant<int32_t, mir::inst::VarId>>
+            load;
+        std::map<mir::inst::VarId, std::variant<int32_t, mir::inst::VarId>>
+            ::iterator it;
         index = 0;
         for (auto& inst : bb.inst) {
           if (index >= call_index[j] && index < upper_bound) {
             auto& i = *inst;
             if (auto x = dynamic_cast<mir::inst::StoreInst*>(&i)) {
+              std::variant<int32_t, mir::inst::VarId> value;
               if (x->val.index() == 1) {
-                it = store.find(std::get<1>(x->val));
-                if (it == store.end()) {
-                  store.insert(
-                      std::map<mir::inst::VarId, mir::inst::VarId>::value_type(
-                          x->dest, std::get<1>(x->val)));
-                } else {
-                  store[x->dest] = std::get<1>(x->val);
-                }
+                value.emplace<1>(std::get<1>(x->val));
+              } else {
+                value.emplace<0>(std::get<0>(x->val));
+              }
+              it = store.find(x->dest);
+              if (it == store.end()) {
+                store.insert(std::map<mir::inst::VarId,
+                                      std::variant<int32_t, mir::inst::VarId>>::
+                                 value_type(x->dest, value));
+              } else {
+                store[x->dest] = value;
               }
             } else if (auto x = dynamic_cast<mir::inst::LoadInst*>(&i)) {
               if (x->src.index() == 1) {
                 it = store.find(std::get<1>(x->src));
                 if (it != store.end()) {
-                  std::map<mir::inst::VarId, mir::inst::VarId>::iterator iter;
+                  std::map<mir::inst::VarId,
+                           std::variant<int32_t, mir::inst::VarId>>::iterator
+                      iter;
                   iter = load.find(x->dest);
                   if (iter == load.end()) {
                     load.insert(
                         std::map<mir::inst::VarId,
-                                 mir::inst::VarId>::value_type(x->dest,
-                                                               it->second));
+                                 std::variant<int32_t, mir::inst::VarId>>::
+                            value_type(x->dest, it->second));
                   } else {
                     load[x->dest] = it->second;
                   }
@@ -74,9 +83,10 @@ class Memory_Var_Propagation : public backend::MirOptimizePass {
           index++;
         }
         for (it = load.begin(); it != load.end(); it++) {
-          std::map<mir::inst::VarId, mir::inst::VarId>::iterator iet;
+          std::map<mir::inst::VarId,
+                   std::variant<int32_t, mir::inst::VarId>>::iterator iet;
           for (iet = load.begin(); iet != load.end(); iet++) {
-            if (it->first == iet->second) {
+            if (iet->second.index() == 1 && it->first == std::get<1>(iet->second)) {
               load[iet->first] = it->second;
             }
           }
@@ -88,67 +98,108 @@ class Memory_Var_Propagation : public backend::MirOptimizePass {
             auto& i = *inst;
             if (auto x = dynamic_cast<mir::inst::AssignInst*>(&i)) {
               if (x->src.index() == 1) {
-                std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
+                std::map<mir::inst::VarId,
+                         std::variant<int32_t, mir::inst::VarId>>::iterator
+                    lit =
                     load.find(std::get<1>(x->src));
                 if (lit != load.end()) {
-                  x->src.emplace<1>(lit->second);
+                  if (lit->second.index() == 0) {
+                    x->src.emplace<0>(std::get<0>(lit->second));
+                  } else {
+                    x->src.emplace<1>(std::get<1>(lit->second));
+                  }
                 }
               }
             } else if (auto x = dynamic_cast<mir::inst::CallInst*>(&i)) {
               for (int j = 0; j < x->params.size(); j++) {
                 if (x->params[j].index() == 1) {
-                  std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
-                      load.find(std::get<1>(x->params[j]));
+                  std::map<mir::inst::VarId,
+                           std::variant<int32_t, mir::inst::VarId>>::iterator
+                      lit = load.find(std::get<1>(x->params[j]));
                   if (lit != load.end()) {
-                    x->params[j].emplace<1>(lit->second);
+                    if (lit->second.index() == 0) {
+                      x->params[j].emplace<0>(std::get<0>(lit->second));
+                    } else {
+                      x->params[j].emplace<1>(std::get<1>(lit->second));
+                    }
                   }
                 }
               }
             } else if (auto x = dynamic_cast<mir::inst::OpInst*>(&i)) {
               if (x->lhs.index() == 1) {
-                std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
-                    load.find(std::get<1>(x->lhs));
+                std::map<mir::inst::VarId,
+                         std::variant<int32_t, mir::inst::VarId>>::iterator
+                    lit = load.find(std::get<1>(x->lhs));
                 if (lit != load.end()) {
-                  x->lhs.emplace<1>(lit->second);
+                  if (lit->second.index() == 0) {
+                    x->lhs.emplace<0>(std::get<0>(lit->second));
+                  } else {
+                    x->lhs.emplace<1>(std::get<1>(lit->second));
+                  }
                 }
               }
               if (x->rhs.index() == 1) {
-                std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
-                    load.find(std::get<1>(x->rhs));
+                std::map<mir::inst::VarId,
+                         std::variant<int32_t, mir::inst::VarId>>::iterator
+                    lit = load.find(std::get<1>(x->rhs));
                 if (lit != load.end()) {
-                  x->rhs.emplace<1>(lit->second);
+                  if (lit->second.index() == 0) {
+                    x->rhs.emplace<0>(std::get<0>(lit->second));
+                  } else {
+                    x->rhs.emplace<1>(std::get<1>(lit->second));
+                  }
                 }
               }
             } else if (auto x = dynamic_cast<mir::inst::LoadInst*>(&i)) {
               if (x->src.index() == 1) {
-                std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
-                    load.find(std::get<1>(x->src));
+                std::map<mir::inst::VarId,
+                         std::variant<int32_t, mir::inst::VarId>>::iterator
+                    lit = load.find(std::get<1>(x->src));
                 if (lit != load.end()) {
-                  x->src.emplace<1>(lit->second);
+                  if (lit->second.index() == 0) {
+                    x->src.emplace<0>(std::get<0>(lit->second));
+                  } else {
+                    x->src.emplace<1>(std::get<1>(lit->second));
+                  }
                 }
               }
             } else if (auto x = dynamic_cast<mir::inst::StoreInst*>(&i)) {
               if (x->val.index() == 1) {
-                std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
-                    load.find(std::get<1>(x->val));
+                std::map<mir::inst::VarId,
+                         std::variant<int32_t, mir::inst::VarId>>::iterator
+                    lit = load.find(std::get<1>(x->val));
                 if (lit != load.end()) {
-                  x->val.emplace<1>(lit->second);
+                  if (lit->second.index() == 0) {
+                    x->val.emplace<0>(std::get<0>(lit->second));
+                  } else {
+                    x->val.emplace<1>(std::get<1>(lit->second));
+                  }
                 }
               }
             } else if (auto x = dynamic_cast<mir::inst::StoreInst*>(&i)) {
               if (x->val.index() == 1) {
-                std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
-                    load.find(std::get<1>(x->val));
+                std::map<mir::inst::VarId,
+                         std::variant<int32_t, mir::inst::VarId>>::iterator
+                    lit = load.find(std::get<1>(x->val));
                 if (lit != load.end()) {
-                  x->val.emplace<1>(lit->second);
+                  if (lit->second.index() == 0) {
+                    x->val.emplace<0>(std::get<0>(lit->second));
+                  } else {
+                    x->val.emplace<1>(std::get<1>(lit->second));
+                  }
                 }
               }
             } else if (auto x = dynamic_cast<mir::inst::PtrOffsetInst*>(&i)) {
               if (x->offset.index() == 1) {
-                std::map<mir::inst::VarId, mir::inst::VarId>::iterator lit =
-                    load.find(std::get<1>(x->offset));
+                std::map<mir::inst::VarId,
+                         std::variant<int32_t, mir::inst::VarId>>::iterator
+                    lit = load.find(std::get<1>(x->offset));
                 if (lit != load.end()) {
-                  x->offset.emplace<1>(lit->second);
+                  if (lit->second.index() == 0) {
+                    x->offset.emplace<0>(std::get<0>(lit->second));
+                  } else {
+                    x->offset.emplace<1>(std::get<1>(lit->second));
+                  }
                 }
               }
             }
