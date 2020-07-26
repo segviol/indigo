b6b1865f4317cee346d403b8b900d5bd4eeef768
hyc <hyc2026@yeah.net>
2020-07-26 16:22:59 +0800

    change namespace

--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ int main(int argc, const char** argv) {
 
   backend::Backend backend(package, options);
   backend.add_pass(
-      std::make_unique<optimization::remove_dead_code::Const_Propagation>());
+      std::make_unique<optimization::const_propagation::Const_Propagation>());
   backend.add_pass(
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   //backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
