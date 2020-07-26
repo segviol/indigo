fbc2c5f2a40baa1e03192d464abe14b25822d38c
hyc <hyc2026@yeah.net>
2020-07-27 00:05:39 +0800

    reopen memvar propagation

--- main.cpp
+++ main.cpp
@@ -78,8 +78,10 @@ int main(int argc, const char** argv) {
   backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
   backend.add_pass(
       std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
-  //backend.add_pass(std::make_unique<
-  //                 optimization::memvar_propagation::Memory_Var_Propagation>());
+  backend.add_pass(std::make_unique<
+                   optimization::memvar_propagation::Memory_Var_Propagation>());
+  backend.add_pass(
+      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
   backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>(5));
   backend.add_pass(std::make_unique<backend::codegen::MathOptimization>());
