65db131a564000df943df6b1b52292fdad1cfad1
starmiku <1207184332@qq.com>
2020-07-26 11:00:26 +0800

    tq_add_simple_algebraic_simplification

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "backend/codegen/codegen.hpp"
 #include "backend/codegen/math_opt.hpp"
 #include "backend/codegen/reg_alloc.hpp"
+#include "backend/optimization/algebraic_simplification.hpp"
 #include "backend/optimization/block_merge.hpp"
 #include "backend/optimization/common_expression_delete.hpp"
 #include "backend/optimization/excess_reg_delete.hpp"
@@ -68,6 +69,9 @@ int main(int argc, const char** argv) {
   LOG(INFO) << ("generating ARM code");
 
   backend::Backend backend(package, options);
+  backend.add_pass(
+      std::make_unique<
+          optimization::algebraic_simplification::AlgebraicSimplification>());
   backend.add_pass(
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
