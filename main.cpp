4dc3c424c5aa8affae423eb2f19ec90c258c0913
Z <tqnwhz@qq.com>
2020-07-29 09:31:21 +0800

    add a dead code remove pass

--- main.cpp
+++ main.cpp
@@ -93,6 +93,8 @@ int main(int argc, const char** argv) {
   backend.add_pass(std::make_unique<optimization::graph_color::Graph_Color>(5));
 
   backend.add_pass(std::make_unique<optimization::cast_inst::Cast_Inst>());
+  backend.add_pass(
+      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
 
   backend.add_pass(std::make_unique<backend::codegen::MathOptimization>());
   backend.add_pass(std::make_unique<backend::codegen::RegAllocatePass>());
