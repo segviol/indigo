79e32473f9cee3738158e726ad18838b083cc644
Rynco Maekawa <lynzrand@outlook.com>
2020-07-30 21:01:02 +0800

    Disable inline

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ void add_passes(backend::Backend& backend) {
   backend.add_pass(std::make_unique<optimization::var_mir_fold::VarMirFold>());
   backend.add_pass(
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
+  // backend.add_pass(std::make_unique<optimization::inlineFunc::Inline_Func>());
   backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
   backend.add_pass(
       std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
