30e744d447e7e64480b8f24dffcbaff114d2cb4a
Z <tqnwhz@qq.com>
2020-07-31 22:42:51 +0800

    Revert "Merge branch 'develop' of..."
    This reverts commit 84bb4d5f7f7f8fea9851a1ecf1370c1adb8baacc

--- backend/codegen/reg_alloc.cpp
+++ backend/codegen/reg_alloc.cpp
@@ -21,7 +21,7 @@ namespace backend::codegen {
 using namespace arm;
 
 const std::set<Reg> GP_REGS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-const std::vector<Reg> TEMP_REGS = {0, 1, 2, 3, 12};
+const std::vector<Reg> TEMP_REGS = {0, 1, 2, 3, 12, REG_LR};
 const std::vector<Reg> GLOB_REGS = {4, 5, 6, 7, 8, 9, 10};
 
 /// An interval represented by this struct is a semi-open interval
