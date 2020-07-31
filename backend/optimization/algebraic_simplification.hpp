20c9fa675321c7b5959a3309ceb2e02c4443652a
starmiku <1207184332@qq.com>
2020-07-31 20:58:02 +0800

    tq support simplification of divided by const

--- backend/optimization/algebraic_simplification.hpp
+++ backend/optimization/algebraic_simplification.hpp
@@ -28,22 +28,27 @@ class AlgebraicSimplification : public backend::MirOptimizePass {
                 }
                 if (!opPtr->lhs.is_immediate() && opPtr->rhs.is_immediate()) {
                   int32_t num = *(opPtr->rhs.get_if<int32_t>());
-                  if (num != 0 && (std::abs(num) & (std::abs(num) - 1)) == 0) {
-                    uint32_t index;
-                    uint32_t mul;
+                  if (num != 0) {
+                    if (num == -1) {
+                      opPtr->op = mir::inst::Op::Sub;
+                      opPtr->rhs = opPtr->lhs;
+                      opPtr->lhs = mir::inst::Value(0);
+                    } else if ((std::abs(num) & (std::abs(num) - 1)) == 0) {
+                      uint32_t index;
 
-                    mul = std::abs(num);
-                    for (index = 0; (mul & 1) == 0; mul >>= 1, index++)
-                      ;
-                    opPtr->op = mir::inst::Op::Shl;
-                    opPtr->rhs = mir::inst::Value(index);
+                      mul = std::abs(num);
+                      for (index = 0; (mul & 1) == 0; mul >>= 1, index++)
+                        ;
+                      opPtr->op = mir::inst::Op::Shl;
+                      opPtr->rhs = mir::inst::Value(index);
 
-                    if (num < 0) {
-                      blksIter->second.inst.insert(
-                          instIter + 1, std::make_unique<mir::inst::OpInst>(
-                                            opPtr->dest, mir::inst::Value(0),
-                                            mir::inst::Value(opPtr->dest),
-                                            mir::inst::Op::Sub));
+                      if (num < 0) {
+                        blksIter->second.inst.insert(
+                            instIter + 1, std::make_unique<mir::inst::OpInst>(
+                                              opPtr->dest, mir::inst::Value(0),
+                                              mir::inst::Value(opPtr->dest),
+                                              mir::inst::Op::Sub));
+                      }
                     }
                   }
                 }
@@ -52,22 +57,79 @@ class AlgebraicSimplification : public backend::MirOptimizePass {
               case mir::inst::Op::Div: {
                 if (!opPtr->lhs.is_immediate() && opPtr->rhs.is_immediate()) {
                   int32_t num = *(opPtr->rhs.get_if<int32_t>());
-                  if (num != 0 && (std::abs(num) & (std::abs(num) - 1)) == 0) {
-                    uint32_t index;
-                    uint32_t mul;
+                  if (num != 0) {
+                    if (num == -1) {
+                      opPtr->op = mir::inst::Op::Sub;
+                      opPtr->rhs = opPtr->lhs;
+                      opPtr->lhs = mir::inst::Value(0);
+                    } else if ((std::abs(num) & (std::abs(num) - 1)) == 0) {
+                      uint32_t index;
+                      uint32_t mul;
 
-                    mul = std::abs(num);
-                    for (index = 0; (mul & 1) == 0; mul >>= 1, index++)
-                      ;
-                    opPtr->op = mir::inst::Op::ShrA;
-                    opPtr->rhs = mir::inst::Value(index);
+                      mul = std::abs(num);
+                      for (index = 0; (mul & 1) == 0; mul >>= 1, index++)
+                        ;
+                      opPtr->op = mir::inst::Op::ShrA;
+                      opPtr->rhs = mir::inst::Value(index);
 
-                    if (num < 0) {
-                      blksIter->second.inst.insert(
-                          instIter + 1, std::make_unique<mir::inst::OpInst>(
-                                            opPtr->dest, mir::inst::Value(0),
-                                            mir::inst::Value(opPtr->dest),
-                                            mir::inst::Op::Sub));
+                      if (num < 0) {
+                        blksIter->second.inst.insert(
+                            instIter + 1, std::make_unique<mir::inst::OpInst>(
+                                              opPtr->dest, mir::inst::Value(0),
+                                              mir::inst::Value(opPtr->dest),
+                                              mir::inst::Op::Sub));
+                      }
+                    } else {
+                      uint32_t numAbs;
+                      int32_t l;
+                      uint64_t m;
+                      int32_t m1;
+                      int32_t dSign;
+                      int32_t shPost;
+
+                      numAbs = std::abs(num);
+                      if ((l = upLog2(numAbs)) < 1)
+                      {
+                        l = 1;
+                      }
+                      m = 1 + ((int64_t)1 << (32 + l + 1)) / numAbs;
+                      m1 = m - ((int64_t)1 << 32);
+                      dSign = (num >= 0 ? 0 : -1);
+                      shPost = l - 1;
+
+                      uint32_t tmp1;
+                      uint32_t tmp2;
+                      uint32_t tmp3;
+
+                      tmp1 = getNewVar(mirFunction);
+                      blksIter->second.inst.insert(instIter + 1, std::unique_ptr<mir::inst::Inst>(new mir::inst::OpInst(
+                        mir::inst::VarId(tmp1), mir::inst::Value(m1), opPtr->lhs, mir::inst::Op::MulSh
+                        )));
+                      tmp2 = getNewVar(mirFunction);
+                      blksIter->second.inst.insert(instIter + 2, std::unique_ptr<mir::inst::Inst>(new mir::inst::OpInst(
+                        mir::inst::VarId(tmp2), mir::inst::Value(mir::inst::VarId(tmp1)), opPtr->lhs, mir::inst::Op::Add
+                        )));
+                      tmp1 = getNewVar(mirFunction);
+                      blksIter->second.inst.insert(instIter + 3, std::unique_ptr<mir::inst::Inst>(new mir::inst::OpInst(
+                        mir::inst::VarId(tmp1), mir::inst::Value(tmp2), mir::inst::Value(shPost), mir::inst::Op::ShrA
+                        )));
+                      tmp2 = getNewVar(mirFunction);
+                      blksIter->second.inst.insert(instIter + 4, std::unique_ptr<mir::inst::Inst>(new mir::inst::OpInst(
+                        mir::inst::VarId(tmp2), opPtr->lhs, mir::inst::Value(31), mir::inst::Op::ShrA
+                        )));
+                      tmp3 = getNewVar(mirFunction);
+                      blksIter->second.inst.insert(instIter + 5, std::unique_ptr<mir::inst::Inst>(new mir::inst::OpInst(
+                        mir::inst::VarId(tmp3), mir::inst::Value(tmp1), mir::inst::Value(tmp2), mir::inst::Op::Sub
+                        )));
+                      tmp1 = getNewVar(mirFunction);
+                      blksIter->second.inst.insert(instIter + 6, std::unique_ptr<mir::inst::Inst>(new mir::inst::OpInst(
+                        mir::inst::VarId(tmp1), mir::inst::Value(tmp3), mir::inst::Value(dSign), mir::inst::Op::Xor
+                        )));
+                      blksIter->second.inst.insert(instIter + 7, std::unique_ptr<mir::inst::Inst>(new mir::inst::OpInst(
+                        opPtr->dest, mir::inst::Value(tmp1), mir::inst::Value(dSign), mir::inst::Op::Sub
+                        )));
+                      
+                      blksIter->second.inst.erase(instIter);
                     }
                   }
                 }
@@ -112,6 +174,37 @@ class AlgebraicSimplification : public backend::MirOptimizePass {
   }
 
  private:
+  // num != 0
+  uint32_t upLog2(uint32_t num) {
+    uint32_t leadZero;
+    uint32_t r;
+
+    for (leadZero = 0;
+         leadZero <= 31 && (num & ((uint32_t)1 << (31 - leadZero))) == 0;
+         leadZero++)
+      ;
+    if ((num & (num - 1)) == 0) {
+      r = 31 - leadZero;
+    } else {
+      r = 32 - leadZero;
+    }
+    return r;
+  }
+
+  uint32_t getNewVar(mir::inst::MirFunction& func)
+  {
+    uint32_t maxId = 1;
+    for (auto& var : func.variables)
+    {
+      if (var.first > maxId)
+      {
+        maxId = var.first;
+      }
+    }
+    func.variables.insert(std::pair<uint32_t, mir::inst::Variable>(++maxId, mir::inst::Variable(
+      mir::types::SharedTyPtr(std::make_shared<mir::types::IntTy>()), false, true)));
+    return maxId;
+  }
 };
 
 }  // namespace optimization::algebraic_simplification
