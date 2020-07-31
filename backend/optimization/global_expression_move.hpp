d17923032460a4dc93151602235b5afe77be4edf
Z <tqnwhz@qq.com>
2020-07-31 15:52:09 +0800

    version 2

--- backend/optimization/global_expression_move.hpp
+++ backend/optimization/global_expression_move.hpp
@@ -10,6 +10,7 @@
 #include <optional>
 #include <set>
 #include <string>
+#include <unordered_set>
 #include <utility>
 #include <variant>
 #include <vector>
@@ -22,14 +23,40 @@
 
 namespace optimization::global_expr_move {
 
+class BlockOps {
+ public:
+  std::set<
+      std::pair<mir::inst::Op, std::pair<mir::inst::Value, mir::inst::Value>>>
+      ops;
+  BlockOps(mir::inst::BasicBlk& blk) {
+    for (auto& inst : blk.inst) {
+      auto& i = *inst;
+      if (inst->inst_kind() == mir::inst::InstKind::Op) {
+        auto opInst = dynamic_cast<mir::inst::OpInst*>(&i);
+        ops.insert({opInst->op, {opInst->lhs, opInst->rhs}});
+      }
+    }
+  }
+
+  bool has_op(
+      std::pair<mir::inst::Op, std::pair<mir::inst::Value, mir::inst::Value>>
+          op) {
+    return ops.count(op);
+  }
+};
 class Env {
  public:
   mir::inst::MirFunction& func;
   livevar_analyse::Livevar_Analyse& lva;
+  std::map<mir::types::LabelId, BlockOps> blk_op_map;
   var_replace::Var_Replace& vp;
   Env(mir::inst::MirFunction& func, livevar_analyse::Livevar_Analyse& lva,
       var_replace::Var_Replace& vp)
-      : func(func), lva(lva), vp(vp) {}
+      : func(func), lva(lva), vp(vp) {
+    for (auto& blkpair : func.basic_blks) {
+      blk_op_map.insert({blkpair.first, BlockOps(blkpair.second)});
+    }
+  }
 };
 std::shared_ptr<Env> env;
 
@@ -48,6 +75,53 @@ class Global_Expr_Mov : public backend::MirOptimizePass {
     }
     return lstart;
   }
+
+  std::unique_ptr<mir::inst::OpInst> copy_opinst(
+      std::unique_ptr<mir::inst::OpInst>& inst) {
+    return std::make_unique<mir::inst::OpInst>(inst->dest, inst->lhs, inst->rhs,
+                                               inst->op);
+  }
+
+  mir::types::LabelId bfs(
+      int start,
+      std::pair<mir::inst::Op, std::pair<mir::inst::Value, mir::inst::Value>>
+          op) {
+    std::list<mir::types::LabelId> queue;
+    std::set<mir::types::LabelId> visited;
+    visited.insert(start);
+    for (auto pre : env->func.basic_blks.at(start).preceding) {
+      if (pre != start) {
+        queue.push_back(pre);
+      }
+    }
+    while (!queue.empty()) {
+      bool all_has_op = true;
+      for (auto pre : queue) {
+        if (!env->blk_op_map.at(pre).has_op(op)) {
+          all_has_op = false;
+          break;
+        }
+      }
+      if (all_has_op) {
+        if (queue.size() == 1) {
+          return queue.front();
+        }
+      }
+      auto front = queue.front();
+      queue.pop_front();
+      if (visited.count(front)) {
+        continue;
+      }
+      visited.insert(front);
+      for (auto pre : env->func.basic_blks.at(front).preceding) {
+        if (visited.count(pre)) {
+          queue.push_back(pre);
+        }
+      }
+    }
+    return start;
+  }
+
   void optimize_func(mir::inst::MirFunction& func) {
     if (func.type->is_extern) {
       return;
@@ -56,74 +130,99 @@ class Global_Expr_Mov : public backend::MirOptimizePass {
     lva.build();
     var_replace::Var_Replace vp(func);
     env = std::make_shared<Env>(func, lva, vp);
-    for (auto& blkpair : func.basic_blks) {
-      if (blkpair.first == 20) {
-        std::cout << 'q';
-      }
-      std::cout << func.name << " - " << blkpair.first << std::endl;
-      auto& block = blkpair.second;
-      auto& blv = lva.livevars[blkpair.first];
-      auto& variables = func.variables;
-      for (auto iter = block.inst.begin(); iter != block.inst.end();) {
-        auto& inst = *iter;
-        if (func.variables.at(inst->dest.id).is_phi_var) {
-          iter++;
+
+    while (true) {
+      bool modify = false;
+      std::list<mir::types::LabelId> queue;
+      std::set<mir::types::LabelId> visited;
+      auto iter = func.basic_blks.end();
+      iter--;
+      queue.push_back(iter->first);
+      while (!queue.empty()) {
+        auto start = queue.front();
+        queue.pop_front();
+        if (visited.count(start)) {
           continue;
         }
-        auto kind = inst->inst_kind();
-        auto& i = *inst;
-        switch (kind) {
-          case mir::inst::InstKind::Op: {
-            auto opInst = dynamic_cast<mir::inst::OpInst*>(&i);
-            if (opInst->lhs.is_immediate() && opInst->rhs.is_immediate()) {
-              break;
-            }
-            int id = -1;
-            if (!opInst->lhs.is_immediate() && !opInst->rhs.is_immediate()) {
-              auto lhs = std::get<mir::inst::VarId>(opInst->lhs);
-              auto rhs = std::get<mir::inst::VarId>(opInst->rhs);
-              if (!blv->live_vars_in->count(lhs) ||
-                  !blv->live_vars_in->count(rhs)) {
-                break;
-              }
-              id = find_common_live_blk(lhs, rhs);
-            } else if (!opInst->lhs.is_immediate()) {
-              auto lhs = std::get<mir::inst::VarId>(opInst->lhs);
-              if (!blv->live_vars_in->count(lhs)) {
-                break;
-              }
-              id = env->vp.defpoint.at(lhs).first;
-
-            } else if (!opInst->rhs.is_immediate()) {
-              auto rhs = std::get<mir::inst::VarId>(opInst->rhs);
-              if (!blv->live_vars_in->count(rhs)) {
-                break;
-              }
-              id = env->vp.defpoint.at(rhs).first;
-            }
-            if (id == blkpair.first) {
-              break;
-            }
-            vp.setdefpoint(inst->dest, blkpair.first,
-                           func.basic_blks.at(id).inst.size());
-            func.variables.at(inst->dest.id).is_temp_var = false;
-            func.basic_blks.at(id).inst.push_back(std::move(inst));
-            iter = block.inst.erase(iter);
-            continue;
-          }
-          // case mir::inst::InstKind::Load: {
-          //   auto loadInst = dynamic_cast<mir::inst::LoadInst*>(&i);
-
-          //   break;
-          // }
-          // case mir::inst::InstKind::Store: {
-          //   auto storeInst = dynamic_cast<mir::inst::StoreInst*>(&i);
-
-          //   break;
-          // }
-          default:;
+        if (start == 6) {
+          std::cout << "as";
         }
-        iter++;
+        auto& blk = func.basic_blks.at(start);
+        {  // visit logic
+          auto& block = blk;
+          auto& blv = lva.livevars[start];
+          auto& variables = func.variables;
+          for (auto iter = block.inst.begin(); iter != block.inst.end();) {
+            auto& inst = *iter;
+            if (func.variables.at(inst->dest.id).is_phi_var) {
+              iter++;
+              continue;
+            }
+            auto kind = inst->inst_kind();
+            auto& i = *inst;
+            switch (kind) {
+              case mir::inst::InstKind::Op: {
+                auto opInst = dynamic_cast<mir::inst::OpInst*>(&i);
+                if (opInst->lhs.is_immediate() && opInst->rhs.is_immediate()) {
+                  break;
+                }
+                int id = -1;
+                if (!opInst->lhs.is_immediate() &&
+                    !opInst->rhs.is_immediate()) {
+                  auto lhs = std::get<mir::inst::VarId>(opInst->lhs);
+                  auto rhs = std::get<mir::inst::VarId>(opInst->rhs);
+                  if (!blv->live_vars_in->count(lhs) ||
+                      !blv->live_vars_in->count(rhs)) {
+                    break;
+                  }
+                  id = bfs(start, {opInst->op, {opInst->lhs, opInst->rhs}});
+                } else if (!opInst->lhs.is_immediate()) {
+                  auto lhs = std::get<mir::inst::VarId>(opInst->lhs);
+                  if (!blv->live_vars_in->count(lhs)) {
+                    break;
+                  }
+                  id = bfs(start, {opInst->op, {opInst->lhs, opInst->rhs}});
+
+                } else if (!opInst->rhs.is_immediate()) {
+                  auto rhs = std::get<mir::inst::VarId>(opInst->rhs);
+                  if (!blv->live_vars_in->count(rhs)) {
+                    break;
+                  }
+                  id = bfs(start, {opInst->op, {opInst->lhs, opInst->rhs}});
+                }
+                if (id == start) {
+                  break;
+                }
+                vp.setdefpoint(inst->dest, start,
+                               func.basic_blks.at(id).inst.size());
+                func.variables.at(inst->dest.id).is_temp_var = false;
+                func.basic_blks.at(id).inst.push_back(std::move(inst));
+                iter = block.inst.erase(iter);
+                modify = true;
+                continue;
+              }
+              // case mir::inst::InstKind::Load: {
+              //   auto loadInst = dynamic_cast<mir::inst::LoadInst*>(&i);
+
+              //   break;
+              // }
+              // case mir::inst::InstKind::Store: {
+              //   auto storeInst = dynamic_cast<mir::inst::StoreInst*>(&i);
+
+              //   break;
+              // }
+              default:;
+            }
+            iter++;
+          }
+        }
+        visited.insert(start);
+        for (auto pre : blk.preceding) {
+          queue.push_back(pre);
+        }
+      }
+      if (!modify) {
+        break;
       }
     }
   }
