32d0a391b5da417a9e57289ed2267d4c6c8135ac
Rynco Maekawa <lynzrand@outlook.com>
2020-08-02 22:17:00 +0800

    Fix has_shift to only be true when holding VarId

--- mir/mir.hpp
+++ mir/mir.hpp
@@ -151,15 +151,17 @@ class Value : public std::variant<int32_t, VarId>, public prelude::Displayable {
       : std::variant<int32_t, VarId>(i),
         shift(shift),
         shift_amount(shift_amount) {}
+
   arm::RegisterShiftKind shift = arm::RegisterShiftKind::Lsl;
   uint8_t shift_amount = 0;
+
   virtual void display(std::ostream& o) const;
 
   template <typename T>
   T* get_if() {
     return std::get_if<T>(this);
   }
-  bool has_shift() const { return shift_amount != 0; }
+  bool has_shift() const { return !is_immediate() && shift_amount != 0; }
   bool is_immediate() const { return std::holds_alternative<int32_t>(*this); }
 };
 
