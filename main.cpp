35a1c938de01d6934e36771fa612743e530e6451
Z <tqnwhz@qq.com>
2020-08-03 10:59:25 +0800

    disable ref count

--- main.cpp
+++ main.cpp
@@ -75,7 +75,7 @@ void add_passes(backend::Backend& backend) {
   backend.add_pass(std::make_unique<optimization::cast_inst::Cast_Inst>());
   backend.add_pass(
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  backend.add_pass(std::make_unique<optimization::ref_count::Ref_Count>());
+  // backend.add_pass(std::make_unique<optimization::ref_count::Ref_Count>());
   backend.add_pass(
       std::make_unique<
           optimization::algebraic_simplification::AlgebraicSimplification>());
