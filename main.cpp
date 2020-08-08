ae61644f33f6899b450042187bd1b94ba5eb8653
Rynco Maekawa <lynzrand@outlook.com>
2020-08-08 11:47:51 +0800

    Add iterative dead code elimination

--- main.cpp
+++ main.cpp
@@ -87,13 +87,12 @@ void add_passes(backend::Backend& backend) {
           optimization::algebraic_simplification::AlgebraicSimplification>());
   backend.add_pass(std::make_unique<
                    optimization::value_shift_collapse::ValueShiftCollapse>());
-  backend.add_pass(
-      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   backend.add_pass(std::make_unique<optimization::exit_ahead::Exit_Ahead>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
   backend.add_pass(std::make_unique<
                    optimization::complex_dce::ComplexDeadCodeElimination>());
-
+  backend.add_pass(
+      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
   backend.add_pass(
       std::make_unique<optimization::graph_color::Graph_Color>(7, true));
