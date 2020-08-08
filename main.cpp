7967829ad315eed003c588b523427a61d9309388
Z <tqnwhz@qq.com>
2020-08-08 16:49:20 +0800

    delete a exit ahead

--- main.cpp
+++ main.cpp
@@ -13,11 +13,8 @@
 #include "backend/optimization/block_merge.hpp"
 #include "backend/optimization/cast_inst.hpp"
 #include "backend/optimization/common_expression_delete.hpp"
-
-#include "backend/optimization/const_loop_expand.hpp"
-
 #include "backend/optimization/complex_dead_code_elimination.hpp"
-
+#include "backend/optimization/const_loop_expand.hpp"
 #include "backend/optimization/const_merge.hpp"
 #include "backend/optimization/const_propagation.hpp"
 #include "backend/optimization/excess_reg_delete.hpp"
@@ -118,17 +115,13 @@ void add_passes(backend::Backend& backend) {
           optimization::algebraic_simplification::AlgebraicSimplification>());
   backend.add_pass(std::make_unique<
                    optimization::value_shift_collapse::ValueShiftCollapse>());
-  backend.add_pass(std::make_unique<optimization::exit_ahead::Exit_Ahead>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
   backend.add_pass(std::make_unique<
                    optimization::complex_dce::ComplexDeadCodeElimination>());
   backend.add_pass(
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
 
-
   backend.add_pass(std::make_unique<optimization::exit_ahead::Exit_Ahead>());
-
-
 
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
   backend.add_pass(
