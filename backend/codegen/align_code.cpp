671fa2dc7ab2b048556d988f22432d29fffe926f
Rynco Maekawa <lynzrand@outlook.com>
2020-08-10 22:23:23 +0800

    Align 7 on cycle start

---
+++ backend/codegen/align_code.cpp
@@ -0,0 +1,47 @@
+#include "align_code.hpp"
+
+#include <algorithm>
+#include <any>
+#include <cassert>
+#include <map>
+#include <memory>
+#include <ostream>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "../optimization/optimization.hpp"
+
+using namespace arm;
+using namespace backend::codegen;
+
+void CodeAlignOptimization::optimize_arm(
+    arm::ArmCode &arm_code, std::map<std::string, std::any> &extra_data_repo) {
+  for (auto &f : arm_code.functions) {
+    optimize_func(*f, extra_data_repo);
+  }
+}
+
+void CodeAlignOptimization::optimize_func(
+    arm::Function &f, std::map<std::string, std::any> &extra_data_repo) {
+  auto cycle_set = std::any_cast<optimization::CycleStartType>(
+                       extra_data_repo.at(optimization::CYCLE_START_DATA_NAME))
+                       .at(f.name);
+  std::vector<std::unique_ptr<arm::Inst>> inst_new;
+  for (auto &i : f.inst) {
+    if (auto x = dynamic_cast<arm::LabelInst *>(&*i)) {
+      if (x->label.find(".bb_") == 0) {
+        try {
+          auto bb_id_idx = x->label.find_last_of("$");
+          auto id_str = x->label.substr(bb_id_idx + 1);
+          auto bb_id = std::stoi(id_str);
+          if (cycle_set.find(bb_id) != cycle_set.end())
+            inst_new.push_back(std::make_unique<CtrlInst>("align", 7, true));
+        } catch (std::exception &i) {
+        }
+      }
+    }
+    inst_new.push_back(std::move(i));
+  }
+  f.inst = std::move(inst_new);
+}
--- backend/optimization/optimization.hpp
+++ backend/optimization/optimization.hpp
@@ -12,6 +12,10 @@ const std::string BASIC_BLOCK_ORDERING_DATA_NAME = "basic_block_ordering";
 using BasicBlockOrderingType =
     std::unordered_map<std::string, std::vector<uint32_t>>;
 
+const std::string CYCLE_START_DATA_NAME = "cycle_start";
+
+using CycleStartType = std::unordered_map<std::string, std::set<uint32_t>>;
+
 const std::string MIR_VARIABLE_TO_ARM_VREG_DATA_NAME = "mir_variable_to_vreg";
 
 using MirVariableToArmVRegType =
