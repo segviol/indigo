254d37b19546d780f6d7a0db6ac6166c398b65da
starmiku <1207184332@qq.com>
2020-08-12 00:04:54 +0800

    statistic compute inst of entry of loop

--- backend/codegen/align_code.cpp
+++ backend/codegen/align_code.cpp
@@ -28,20 +28,45 @@ void CodeAlignOptimization::optimize_func(
                        extra_data_repo.at(optimization::CYCLE_START_DATA_NAME))
                        .at(f.name);
   std::vector<std::unique_ptr<arm::Inst>> inst_new;
-  for (auto &i : f.inst) {
-    if (auto x = dynamic_cast<arm::LabelInst *>(&*i)) {
+  for (size_t index = 0; index < f.inst.size(); index++) {
+    auto &i = f.inst.begin() + index;
+    if (auto x = dynamic_cast<arm::LabelInst *>(i->get())) {
       if (x->label.find(".bb_") == 0) {
-        try {
-          auto bb_id_idx = x->label.find_last_of("$");
-          auto id_str = x->label.substr(bb_id_idx + 1);
-          auto bb_id = std::stoi(id_str);
-          if (cycle_set.find(bb_id) != cycle_set.end())
-            inst_new.push_back(std::make_unique<CtrlInst>("align", 7, true));
-        } catch (std::exception &i) {
+        uint32_t cmpuInstNum;
+        uint32_t totalInstNum;
+
+        cmpuInstNum = 0;
+        for (totalInstNum = 0;
+             totalInstNum < 5 && index + totalInstNum < f.inst.size();
+             totalInstNum++) {
+          arm::Inst *inst = (f.inst.begin() + index + totalInstNum)->get();
+          if (inst->op == arm::OpCode::LdR || inst->op == arm::OpCode::LdM ||
+              inst->op == arm::OpCode::StR || inst->op == arm::OpCode::StM ||
+              inst->op == arm::OpCode::Push || inst->op == arm::OpCode::Pop) {
+          } else if (inst->op == arm::OpCode::B ||
+                     inst->op == arm::OpCode::Bl ||
+                     inst->op == arm::OpCode::Bx ||
+                     inst->op == arm::OpCode::Cbz ||
+                     inst->op == arm::OpCode::Cbnz) {
+            break;
+          } else {
+            cmpuInstNum++;
+          }
+        }
+
+        if (totalInstNum >= 3 && (double)cmpuInstNum / totalInstNum > 0.6) {
+          try {
+            auto bb_id_idx = x->label.find_last_of("$");
+            auto id_str = x->label.substr(bb_id_idx + 1);
+            auto bb_id = std::stoi(id_str);
+            if (cycle_set.find(bb_id) != cycle_set.end())
+              inst_new.push_back(std::make_unique<CtrlInst>("align", 7, true));
+          } catch (std::exception &i) {
+          }
         }
       }
     }
-    inst_new.push_back(std::move(i));
+    inst_new.push_back(std::move(*i));
   }
   f.inst = std::move(inst_new);
 }
