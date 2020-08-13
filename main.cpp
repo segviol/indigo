b066191f14bc44a6a5e200cda6a24d1567c05000
Z <tqnwhz@qq.com>
2020-08-13 11:21:08 +0800

    disable loop unrolling

--- main.cpp
+++ main.cpp
@@ -84,12 +84,12 @@ void add_passes(backend::Backend& backend) {
   //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   backend.add_pass(
       std::make_unique<optimization::loop_expand::Const_Loop_Expand>());
-  backend.add_pass(
-      std::make_unique<optimization::loop_unrolling::Loop_Unrolling>());
-  backend.add_pass(
-      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
-  backend.add_pass(
-      std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
+  // backend.add_pass(
+  //     std::make_unique<optimization::loop_unrolling::Loop_Unrolling>());
+  // backend.add_pass(
+  //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
+  // backend.add_pass(
+  //     std::make_unique<optimization::common_expr_del::Common_Expr_Del>());
   backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
   backend.add_pass(
       std::make_unique<optimization::const_propagation::Const_Propagation>());
