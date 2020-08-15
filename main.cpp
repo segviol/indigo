f89ddf2a4a34649162919500e7c300871cbce5ba
Rynco Maekawa <lynzrand@outlook.com>
2020-08-15 10:54:15 +0800

    Add one more merge block

--- main.cpp
+++ main.cpp
@@ -137,6 +137,7 @@ void add_passes(backend::Backend& backend) {
       std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   // backend.add_pass(std::make_unique<optimization::cycle::Cycle>());
   backend.add_pass(std::make_unique<optimization::exit_ahead::Exit_Ahead>());
+  backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
   backend.add_pass(
       std::make_unique<optimization::func_array_global::Func_Array_Global>());
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
