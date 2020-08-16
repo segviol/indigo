3bb93740783f4bb77c5d2a1b0825c72f927f3d18
Rynco Maekawa <lynzrand@outlook.com>
2020-08-16 16:08:13 +0800

    Temporarily disable remove dead code below

--- main.cpp
+++ main.cpp
@@ -132,8 +132,8 @@ void add_passes(backend::Backend& backend) {
   backend.add_pass(std::make_unique<backend::codegen::BasicBlkRearrange>());
   backend.add_pass(std::make_unique<
                    optimization::complex_dce::ComplexDeadCodeElimination>());
-  backend.add_pass(
-      std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
+  // backend.add_pass(
+  //     std::make_unique<optimization::remove_dead_code::Remove_Dead_Code>());
   // backend.add_pass(std::make_unique<optimization::cycle::Cycle>());
   // backend.add_pass(std::make_unique<optimization::exit_ahead::Exit_Ahead>());
   backend.add_pass(std::make_unique<optimization::mergeBlocks::Merge_Block>());
