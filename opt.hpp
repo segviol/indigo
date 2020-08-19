b47b49af71b44805925c9376ce6c16e51df08f22
Rynco Maekawa <lynzrand@outlook.com>
2020-08-19 09:45:05 +0800

    Disable conditional exec

--- opt.hpp
+++ opt.hpp
@@ -8,6 +8,7 @@ struct Options {
   std::string in_file;
   std::string out_file;
   bool verbose;
+  bool allow_conditional_exec;
   bool show_code_after_each_pass;
   bool dry_run;
   std::optional<std::set<std::string>> run_pass;
