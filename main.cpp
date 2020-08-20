38ce52e6b455b1ef5d3e802091c10056bedd78a1
Rynco Maekawa <lynzrand@outlook.com>
2020-08-20 10:12:50 +0800

    Add LF

--- main.cpp
+++ main.cpp
@@ -375,28 +375,28 @@ Options parse_options(int argc, const char** argv) {
 }
 
 std::string file_header =
-    ".syntax unified\
-	.eabi_attribute	67, \"2.09\"	@ Tag_conformance\
-	.cpu	cortex-a7\
-	.eabi_attribute	6, 10	@ Tag_CPU_arch\
-	.eabi_attribute	7, 65	@ Tag_CPU_arch_profile\
-	.eabi_attribute	8, 1	@ Tag_ARM_ISA_use\
-	.eabi_attribute	9, 2	@ Tag_THUMB_ISA_use\
-	.fpu	neon-vfpv4\
-	.eabi_attribute	36, 1	@ Tag_FP_HP_extension\
-	.eabi_attribute	42, 1	@ Tag_MPextension_use\
-	.eabi_attribute	44, 2	@ Tag_DIV_use\
-	.eabi_attribute	34, 1	@ Tag_CPU_unaligned_access\
-	.eabi_attribute	68, 3	@ Tag_Virtualization_use\
-	.eabi_attribute	17, 1	@ Tag_ABI_PCS_GOT_use\
-	.eabi_attribute	20, 1	@ Tag_ABI_FP_denormal\
-	.eabi_attribute	21, 1	@ Tag_ABI_FP_exceptions\
-	.eabi_attribute	23, 3	@ Tag_ABI_FP_number_model\
-	.eabi_attribute	24, 1	@ Tag_ABI_align_needed\
-	.eabi_attribute	25, 1	@ Tag_ABI_align_preserved\
-	.eabi_attribute	28, 1	@ Tag_ABI_VFP_args\
-	.eabi_attribute	38, 1	@ Tag_ABI_FP_16bit_format\
-	.eabi_attribute	18, 4	@ Tag_ABI_PCS_wchar_t\
-	.eabi_attribute	26, 2	@ Tag_ABI_enum_size\
-	.eabi_attribute	14, 0	@ Tag_ABI_PCS_R9_use\
+    ".syntax unified\n\
+	.eabi_attribute	67, \"2.09\"	@ Tag_conformance\n\
+	.cpu	cortex-a7\n\
+	.eabi_attribute	6, 10	@ Tag_CPU_arch\n\
+	.eabi_attribute	7, 65	@ Tag_CPU_arch_profile\n\
+	.eabi_attribute	8, 1	@ Tag_ARM_ISA_use\n\
+	.eabi_attribute	9, 2	@ Tag_THUMB_ISA_use\n\
+	.fpu	neon-vfpv4\n\
+	.eabi_attribute	36, 1	@ Tag_FP_HP_extension\n\
+	.eabi_attribute	42, 1	@ Tag_MPextension_use\n\
+	.eabi_attribute	44, 2	@ Tag_DIV_use\n\
+	.eabi_attribute	34, 1	@ Tag_CPU_unaligned_access\n\
+	.eabi_attribute	68, 3	@ Tag_Virtualization_use\n\
+	.eabi_attribute	17, 1	@ Tag_ABI_PCS_GOT_use\n\
+	.eabi_attribute	20, 1	@ Tag_ABI_FP_denormal\n\
+	.eabi_attribute	21, 1	@ Tag_ABI_FP_exceptions\n\
+	.eabi_attribute	23, 3	@ Tag_ABI_FP_number_model\n\
+	.eabi_attribute	24, 1	@ Tag_ABI_align_needed\n\
+	.eabi_attribute	25, 1	@ Tag_ABI_align_preserved\n\
+	.eabi_attribute	28, 1	@ Tag_ABI_VFP_args\n\
+	.eabi_attribute	38, 1	@ Tag_ABI_FP_16bit_format\n\
+	.eabi_attribute	18, 4	@ Tag_ABI_PCS_wchar_t\n\
+	.eabi_attribute	26, 2	@ Tag_ABI_enum_size\n\
+	.eabi_attribute	14, 0	@ Tag_ABI_PCS_R9_use\n\
 ";
