2cb6d8667d9e199abe9bceba8c3e56849ca3cef2
Rynco Maekawa <lynzrand@outlook.com>
2020-08-20 09:54:26 +0800

    Add file header & sdiv

--- main.cpp
+++ main.cpp
@@ -58,6 +58,8 @@ Options global_options;
 string read_input(std::string&);
 Options parse_options(int argc, const char** argv);
 
+std::string file_header;
+
 void add_passes(backend::Backend& backend) {
   backend.add_pass(std::make_unique<optimization::sanity_check::SanityCheck>());
   backend.add_pass(
@@ -233,7 +235,7 @@ int main(int argc, const char** argv) {
     LOG(INFO) << "writing to output file: " << options.out_file;
 
     ofstream output_file(options.out_file);
-    output_file << code << std::endl;
+    output_file << file_header << std::endl << code << std::endl;
     return 0;
   }
 }
@@ -371,3 +373,31 @@ Options parse_options(int argc, const char** argv) {
 
   return std::move(options);
 }
+
+std::string file_header =
+    "	.text\
+	.syntax unified\
+	.eabi_attribute	67, \"2.09\"	@ Tag_conformance\
+	.cpu	cortex-a7\
+	.eabi_attribute	6, 10	@ Tag_CPU_arch\
+	.eabi_attribute	7, 65	@ Tag_CPU_arch_profile\
+	.eabi_attribute	8, 1	@ Tag_ARM_ISA_use\
+	.eabi_attribute	9, 2	@ Tag_THUMB_ISA_use\
+	.fpu	neon-vfpv4\
+	.eabi_attribute	36, 1	@ Tag_FP_HP_extension\
+	.eabi_attribute	42, 1	@ Tag_MPextension_use\
+	.eabi_attribute	44, 2	@ Tag_DIV_use\
+	.eabi_attribute	34, 1	@ Tag_CPU_unaligned_access\
+	.eabi_attribute	68, 3	@ Tag_Virtualization_use\
+	.eabi_attribute	17, 1	@ Tag_ABI_PCS_GOT_use\
+	.eabi_attribute	20, 1	@ Tag_ABI_FP_denormal\
+	.eabi_attribute	21, 1	@ Tag_ABI_FP_exceptions\
+	.eabi_attribute	23, 3	@ Tag_ABI_FP_number_model\
+	.eabi_attribute	24, 1	@ Tag_ABI_align_needed\
+	.eabi_attribute	25, 1	@ Tag_ABI_align_preserved\
+	.eabi_attribute	28, 1	@ Tag_ABI_VFP_args\
+	.eabi_attribute	38, 1	@ Tag_ABI_FP_16bit_format\
+	.eabi_attribute	18, 4	@ Tag_ABI_PCS_wchar_t\
+	.eabi_attribute	26, 2	@ Tag_ABI_enum_size\
+	.eabi_attribute	14, 0	@ Tag_ABI_PCS_R9_use\
+"
